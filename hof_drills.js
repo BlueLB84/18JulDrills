47c583ed402d22dfb63488560f675b36e1bd0bdb
L Barbee <bluelb84@gmail.com>
2017-07-18 19:32:17 -0400

    fixed solution for first drill

--- hof_drills.js
+++ hof_drills.js
@@ -1,18 +1,17 @@
 // Functions as arguments (1)
-// let hello = () => 'Hello world';
-// let goodbye = () => 'Goodbye world';
 function hello() {
-    console.log('Hello world');
+    return 'Hello world';
 }
 
 function goodbye() {
-    console.log('Goodbye world');
+    return 'Goodbye world';
 }
 
 function repeat(fn, n) {
-    for (let i = 0; i < n; i++) {
-        fn();
+    for (let i = 1; i <= n; i++) {
+        console.log(fn());
     }
 }
 
-console.log(repeat(hello, 5));
+repeat(hello, 5);
+repeat(goodbye, 5);
